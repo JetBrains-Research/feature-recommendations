727058906a7dcdc9a7299ea8c07e721f913c3f6e
Mariia Eliseeva <eliseevamary17@gmail.com>
2020-03-24 18:03:30 +0300

    update quiality evaluation code

--- quality_evaluation.py
+++ quality_evaluation.py
@@ -1,14 +1,16 @@
-import operator
 from tqdm import tqdm
 
 from reader import read_events_raw, event_to_tips
 from constants import INPUT_FILE_NAME, TIPS_GROUP, ACTION_INVOKED_GROUP, PREDICTED_TIME_MILLIS
 
+# INPUT_FILE_NAME = "./log_sample_with_answers_full.csv"
 
-def evaluate(events):
+
+def evaluate(events, device_cnt):
     all_tips = 0  # do dictionary when algorithm name added to logs
     useful_tips = 0
     device_to_tips = {}
+    tip_types = {}
     for event in tqdm(events):
         device_id, group_id, event_id, timestamp, count, bucket = event
         if group_id == TIPS_GROUP:
@@ -16,7 +18,10 @@ def evaluate(events):
                 device_to_tips[device_id] = []
             device_to_tips[device_id].append((event_id, timestamp))
             all_tips += 1
+            tip_types[event_id] = True
 
+    good_devices = {}
+    good_tips = {}
     for event in tqdm(events):
         device_id, group_id, event_id, timestamp, count, bucket = event
         if group_id == ACTION_INVOKED_GROUP:
@@ -25,13 +30,21 @@ def evaluate(events):
                 showed_tips = device_to_tips[device_id]
                 for elem in showed_tips:
                     tip, tip_timestamp = elem
-                    if tip in possible_tips and timestamp > tip_timestamp and (timestamp - tip_timestamp) < PREDICTED_TIME_MILLIS:
+                    if tip in possible_tips and timestamp > tip_timestamp \
+                            and (timestamp - tip_timestamp) < PREDICTED_TIME_MILLIS:
                         useful_tips += 1
+                        good_devices[device_id] = True
+                        good_tips[tip] = True
 
-    return useful_tips / all_tips
+    accuracy = useful_tips / all_tips
+    users_accuracy = len(list(good_devices.keys())) * 1. / device_cnt
+    tips_accuracy = len(list(good_tips.keys())) * 1. / len(list(tip_types.keys()))
+    return accuracy, users_accuracy, tips_accuracy
 
 
 if __name__ == "__main__":
     events, event_types, devices = read_events_raw(INPUT_FILE_NAME)
-    accuracy = evaluate(events)
+    accuracy, users_accuracy, tips_accuracy = evaluate(events, len(devices))
     print(f"Accuracy is: {accuracy}")
+    print(f"Percent of users who followed the tips is: {users_accuracy}")
+    print(f"Percent of tips that were used al least once: {tips_accuracy}")
