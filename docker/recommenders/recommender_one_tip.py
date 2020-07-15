bad2920069f1f729d613a766ddffe0c39c336263
MariEliseeva <eliseevamary17@gmail.com>
2020-07-15 10:34:09 +0300

    add tip name in the one tip algo

--- docker/recommenders/recommender_one_tip.py
+++ docker/recommenders/recommender_one_tip.py
@@ -11,11 +11,21 @@ class RecommenderOneTip(Recommender):
         if is_logging:
             logging.info("RecommenderOneTip:init: init started.")
         super(RecommenderOneTip, self).__init__(None, None, None, is_logging)
+        self.the_tip = "neue-smart_selection.html"
 
     def recommend(self, test_device_events, tips):
         if self.is_logging:
             logging.info("RecommenderOneTip:recommend: recommendation started.")
         random.shuffle(tips)
+        new_tips = []
+        is_tip_met = False
+        for tip in tips:
+            if self.the_tip == tip:
+                is_tip_met = True
+            else:
+                new_tips.append(tip)
+        if is_tip_met:
+            new_tips = [self.the_tip] + new_tips
         if self.is_logging:
             logging.info("RecommenderOneTip:recommend: recommendation made.")
-        return tips
+        return new_tips
